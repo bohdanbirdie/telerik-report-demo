a9047f517a1647ef9b091e79af823e968b5b5b7a
bohdanbirdie <voja4ok@yahoo.com>
2020-04-06 15:30:20 +0300

    Updates

---
+++ precache-manifest.07cd53e2fe9d22529aa967ef33f55b21.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "17c811f738803e4d1a48",
+    "url": "/telerik-report-demo/static/js/runtime~main.17c811f7.js"
+  },
+  {
+    "revision": "d7a99106407e02c02743",
+    "url": "/telerik-report-demo/static/js/main.d7a99106.chunk.js"
+  },
+  {
+    "revision": "63fb5b41de279ed09cdd",
+    "url": "/telerik-report-demo/static/js/1.63fb5b41.chunk.js"
+  },
+  {
+    "revision": "d7a99106407e02c02743",
+    "url": "/telerik-report-demo/static/css/main.9f9fe371.chunk.css"
+  },
+  {
+    "revision": "41bbc483270d0848c66f1832ed12e661",
+    "url": "/telerik-report-demo/index.html"
+  }
+];
